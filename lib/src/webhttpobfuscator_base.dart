4dc8d1cdf0a8853c126fb22f6c1e3bd38b6e5b65
Binozo <entwickler@binozoworks.de>
2022-11-26 19:22:13 +0000

    added httpobfuscator class

--- lib/src/webhttpobfuscator_base.dart
+++ lib/src/webhttpobfuscator_base.dart
@@ -1,6 +1,23 @@
 // TODO: Put public facing types in this file.
 
+import 'package:dio/dio.dart';
+
 /// Checks if you are awesome. Spoiler: you are.
 class Awesome {
   bool get isAwesome => true;
+}
+
+class HttpObfuscatorClient with DioMixin {
+
+  @override
+  Future<Response<T>> get<T>(String path,
+      {Map<String, dynamic>? queryParameters,
+      Options? options,
+      CancelToken? cancelToken,
+      ProgressCallback? onReceiveProgress}) async {
+
+
+
+    return Response(requestOptions: RequestOptions(path: path));
+  }
 }
