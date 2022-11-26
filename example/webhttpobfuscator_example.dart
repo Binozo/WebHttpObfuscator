ddfd0e2334b30b7f333da645f6c23e8c9bc5979c
Binozo <entwickler@binozoworks.de>
2022-11-26 18:08:45 +0000

    initial commit

---
+++ example/webhttpobfuscator_example.dart
@@ -0,0 +1,6 @@
+import 'package:webhttpobfuscator/webhttpobfuscator.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/webhttpobfuscator.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library webhttpobfuscator;
+
+export 'src/webhttpobfuscator_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
