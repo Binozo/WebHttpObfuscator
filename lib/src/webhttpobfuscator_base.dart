a22929cfc8d3645e20a470ad77beda1485e53d38
Binozo <entwickler@binozoworks.de>
2022-11-27 21:42:37 +0100

    modified class

--- lib/src/webhttpobfuscator_base.dart
+++ lib/src/webhttpobfuscator_base.dart
@@ -1,16 +1,28 @@
 // TODO: Put public facing types in this file.
 
-import 'package:dio/dio.dart';
+import 'dart:convert';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
-}
+import 'package:dio/dio.dart';
+import 'package:webhttpobfuscator/connector/connector.dart';
+import 'package:webhttpobfuscator/connector/obfuscator_response.dart';
 
 class HttpObfuscatorClient with DioMixin {
   final String _obfuscatorServerUrl;
+  final Function(String) _payloadEncryptor;
+  final Function(String) _payloadDecryptor;
 
-  HttpObfuscatorClient(this._obfuscatorServerUrl);
+  final BaseOptions baseOptions;
+  HttpObfuscatorClient(this._obfuscatorServerUrl, this._payloadEncryptor, this._payloadDecryptor, this.baseOptions);
+
+  String _convertRequestToJson(String url, String requestMethod, Map<String, dynamic> headers, dynamic payload) {
+    final Map<String, dynamic> data = {
+      "method" : requestMethod,
+      "url" : url,
+      "headers" : headers,
+      "payload" : payload
+    };
+    return jsonEncode(data);
+  }
 
   @override
   Future<Response<T>> get<T>(String path,
