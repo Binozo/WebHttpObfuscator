c99aa24d79599f273ecf50a2607ddbcca3ecd919
Binozo <entwickler@binozoworks.de>
2022-11-28 16:04:53 +0100

    obfuscator_response.dart header converter bugfix

--- lib/connector/obfuscator_response.dart
+++ lib/connector/obfuscator_response.dart
@@ -3,14 +3,14 @@ import 'package:dio/dio.dart';
 class ObfuscatorResponse {
   final int _responseCode;
   final String _body;
-  final Map<String, String> _headers;
+  final Map<String, dynamic> _headers;
 
-  ObfuscatorResponse.fromJson(Map<String, dynamic> json) : _responseCode = json["code"], _body = json["body"], _headers = Map<String, String>.from(json["headers"]);
+  ObfuscatorResponse.fromJson(Map<String, dynamic> json) : _responseCode = json["code"], _body = json["body"], _headers = Map<String, dynamic>.from(json["headers"]);
 
   Headers get headers =>
       Headers.fromMap(
           _headers.map(
-                  (key, value) => MapEntry(key, List<String>.from([value])))); // bit of a hack
+                  (key, value) => MapEntry(key, List<String>.from([...value])))); // bit of a hack
 
   String get body => _body;
 
